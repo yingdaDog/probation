28594ec390522288c3542b62a0c9740b3d5d8a7a
DogWin <yingdamingjing@gmail.com>
2020-12-03 19:12:30 +0800

    update add other

---
+++ work_week1/html/week1/js/all.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    console.log('ready');
+
+    
+  
+    $('.js-to-top').click(function(){
+        $('body,html').animate({scrollTop : 0},700,'swing');
+    })
+    let posY;
+    $(document).scroll(function() {
+        posY = $('html').scrollTop();
+        if(posY >= 400){return $('.js-to-top').addClass('active');}
+        else {return $('.js-to-top').removeClass('active');}
+    })
+
+    var swiper = new Swiper('.swiper-container', {
+        loop : true,
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      });
+    
+})
